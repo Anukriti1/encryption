5e099dd0242ced4eadc1e9b58c11c5385da02930
Bhimasen <brautaray@enbake.com>
2016-06-20 18:00:44 +0530

    Added api for List all employee

--- api/managerApi.js
+++ api/managerApi.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 var queryServe =  require('./mssql_quries.js');
 
-// input comes as compney id from front end and manager can fetch it
+// All project list for a compnay
 var allProjects = function(req, res) {
 	// auth query with username/Password
 	if(req.body && req.body.CompanyId){
@@ -19,5 +19,21 @@ var allProjects = function(req, res) {
 		res.status(401).json({});
 	}	
 }
+// this api is for getting all employee list for a company
+var allEmp = function(req, res) {
+	// auth query with username/Password
+	if(req.body && req.body.CompanyId){
+		var data = {};
+		data.input = {'CompanyId':req.body.CompanyId};
+		data.query = 'SELECT * FROM Employees WHERE CompanyId = @CompanyId';
+		// sending queries to db
+		queryServe.sqlServe(data,function(resData){
+			res.status(200).json(resData);
+		});
+	} else {
+		res.status(401).json({});
+	}	
+}
 router.post('/allProjectsList',allProjects);
+router.post('/allEmp',allEmp);
 module.exports = router;
