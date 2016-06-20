7d7e8a62a5c3cb0347d400709271402ed8bb7370
Bhimasen <brautaray@enbake.com>
2016-06-20 17:50:47 +0530

    create an export function

--- api/mssql_quries.js
+++ api/mssql_quries.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var express = require('express');
-var router = express.Router();
 var sql = require('mssql');
 var async = require('async');
 var db_config = require('../config/db_config');
@@ -12,28 +11,25 @@ var connect_query = db_config.db.type+"://"+db_config.db.username+':'
 This is the root funtion for all queries from ms sql
 example request {"input":{"id":2},"query":"select * from Employees where id = @id"}
 **/
-var sqlServe = function(req,res){
-	console.log(req.body)
+exports.sqlServe = function(data,callbackRes){
 	sql.connect(connect_query).then(function() {
 		var request = new sql.Request();
-		if(req.body.query){
+		if(data.query){
 			// cheking for input 
-			createReqInp(req.body.input,request,function(){
-				request.query(req.body.query, function(err, recordset) {
+			createReqInp(data.input,request,function(){
+				request.query(data.query, function(err, recordset) {
 				    if(err){
 				    	console.log(err);
-				    	res.status(401).json({'message' : 'error in database query'});
+				    	callbackRes({'message' : 'error in database query'});
 				    }
-				    // closing connection
-				    sql.close();
-				    res.status(200).json(recordset);
+				    callbackRes(recordset);
 				});
 			})
 		} else {
-			res.status(401).json({'message' : 'missing query'});
+			callbackRes({'message' : 'missing query'});
 		}
 	}).catch(function(err) {
-		res.status(401).json({'message' : 'error in database connection'});
+		callbackRes({'message' : 'error in database connection'});
 		console.log(err)
 	});
 }
@@ -46,7 +42,7 @@ var createReqInp = function(input,request,callback){
 		callback();
 	} else {
 		var inps = Object.keys(input);
-		async.forEach(inps,function(inp, callbackA){ 
+		async.forEach(inps,function(inp, callbackA){
 			request.input(inp, input[inp]);
 			callbackA();
 		},function(){
@@ -54,5 +50,4 @@ var createReqInp = function(input,request,callback){
 		})
 	}
 }
-router.post('/', sqlServe);
-module.exports = router;
+module.exports = exports;
