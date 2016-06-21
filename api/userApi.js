bb2139fe47c4bdc817e774fa638a898993c74550
Bhimasen <brautaray@enbake.com>
2016-06-21 13:35:26 +0530

    Login With Pin api

--- api/userApi.js
+++ api/userApi.js
@@ -8,7 +8,7 @@ var queryServe =  require('./mssql_quries.js');
 // for login api
 var logIn = function(req, res) {
 	// auth query with username/Password
-	if(req.body && req.body.username && req.body.password){
+	if (req.body && req.body.username && req.body.password){
 		var data = {};
 		data.input = {'LoginUserId':req.body.username,'LoginPassword': req.body.password};
 		data.query = 'SELECT * FROM LoginUser INNER JOIN Employees ON LoginUser.EmployeeId = Employees.Id WHERE LoginUserId = @LoginUserId AND LoginPassword = @LoginPassword';
@@ -21,6 +21,24 @@ var logIn = function(req, res) {
 		res.status(401).json({});
 	}	
 }
+// login with pin api
+var loginPin = function(req, res){
+	if(req.body && req.body.pin && req.body.username && req.body.password){
+		var data = {};
+		data.input = {'LoginUserId':req.body.username,'LoginPassword': req.body.password, 'Pincode' : req.body.pin};
+		data.query = 'SELECT * FROM LoginUser INNER JOIN EmployeePincode ON LoginUser.EmployeeId = EmployeePincode.EmployeeId INNER JOIN Employees ON LoginUser.EmployeeId = Employees.Id'
+					 +' WHERE LoginUserId = @LoginUserId AND LoginPassword = @LoginPassword AND  Pincode = @Pincode';
+		console.log(data)
+		queryServe.sqlServe(data,function(resData){
+			console.log(resData);
+			res.status(200).json(resData);
+		});
+	} else {
+		res.status(401).json({});
+	}
+}
 
+// assign apis to router
 router.post('/login', logIn);
+router.post('/loginPin', loginPin);
 module.exports = router;
