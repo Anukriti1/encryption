92a300db4f989b267c4e5d8b1c110acf47d8b133
Bhimasen <brautaray@enbake.com>
2016-06-28 16:22:09 +0530

    config update

--- config/db_config.js
+++ config/db_config.js
@@ -6,7 +6,12 @@ module.exports = {
 		password : 'testing123',
 		database_name : 'DB_A059D9_rtailwal',
 		host : 'SQL5027.Smarterasp.net',
-		connectionTimeout : 45000,
-		requestTimeout : 45000
+		connectionTimeout : 99000,
+		requestTimeout : 99000,
+		pool: {
+	        max: 50,
+	        min: 0,
+	        idleTimeoutMillis: 30000
+	    }
 	}
 }
